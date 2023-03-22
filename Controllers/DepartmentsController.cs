baf5d13a039508eb0fb62567e586f4032440741f
Elisheva-S <e0527682635@gmail.com>
2023-03-22 22:02:29 +0200

    Add project files.

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DAL;
+using System.Data;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Runtime.Intrinsics.Arm;
+using Model;
+using Entities;
+
+namespace WebApi_ServerSide.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        Department_entity departmentEntity = new Department_entity();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("getAllDepartments")]
+
+        public JsonResult Get()
+        {
+            return new JsonResult(departmentEntity.GetAllDepartments());
+        }
+        [HttpPost("postDepartment")]
+        public JsonResult Post([FromBody] Department department)
+        {
+            return new JsonResult(departmentEntity.PostDepartment(department));            
+        }
+
+        [HttpPut("editDepartment")]
+        public JsonResult Put([FromBody] Department department)
+        {
+            return new JsonResult(departmentEntity.EditDep(department));
+        }
+
+        [HttpDelete("deleteDepatrment")]
+        public JsonResult Delete(int departmentId)
+        {
+            return new JsonResult(departmentEntity.DeleteDep(departmentId));           
+        }
+        
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Data;
+
+namespace WebApi_ServerSide.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product_entity ProductEntity = new Product_entity();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("getAllProducts")]
+
+        public JsonResult Get()
+        {
+            return new JsonResult(ProductEntity.GetAllProducts());
+        }
+        [HttpPost("postProduct")]
+        public JsonResult Post([FromBody] Product Product)
+        {
+            return new JsonResult(ProductEntity.PostProduct(Product));
+        }
+        [HttpPut("EditProduct")]
+        public JsonResult Put([FromBody] Product Product)
+        {
+            return new JsonResult (ProductEntity.EditProduct(Product));           
+        }
+        [HttpDelete("deleteProduct")]
+        public JsonResult Delete(int ProductId)
+        {
+            return new JsonResult(ProductEntity.DeleteProduct(ProductId));
+        }
+    }
+}
